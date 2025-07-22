93f2a901d4a0ed1f9e8ec474702a9ad134a791a9
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2025-07-22 16:24:49 -0300

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <div class="bg-white p-6 text-text-primary space-y-6 rounded-2xl shadow-lg shadow-slate-200">
+    <p class="text-xl">
+      <slot name="card-title" />
+    </p>
+    <p>
+      <slot />
+    </p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+import Card from '@/components/Card.vue';
 import Hero from '@/components/Hero.vue';
 
 </script>
 
 <template>
   <Hero />
+  <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-6 container mx-auto ">
+    <Card>
+      <template #card-title>Title</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat debitis necessitatibus provident doloremque error,
+      repudiandae dolorum quaerat magnam, nihil adipisci ut veniam natus totam reiciendis exercitationem doloribus.
+      Recusandae, minus architecto.
+    </Card>
+    <Card>
+      <template #card-title>Title</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat debitis necessitatibus provident doloremque error,
+      repudiandae dolorum quaerat magnam, nihil adipisci ut veniam natus totam reiciendis exercitationem doloribus.
+      Recusandae, minus architecto.
+    </Card>
+    <Card>
+      <template #card-title>Title</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat debitis necessitatibus provident doloremque error,
+      repudiandae dolorum quaerat magnam, nihil adipisci ut veniam natus totam reiciendis exercitationem doloribus.
+      Recusandae, minus architecto.
+    </Card>
+    <Card>
+      <template #card-title>Title</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat debitis necessitatibus provident doloremque error,
+      repudiandae dolorum quaerat magnam, nihil adipisci ut veniam natus totam reiciendis exercitationem doloribus.
+      Recusandae, minus architecto.
+    </Card>
+  </div>
 </template>
